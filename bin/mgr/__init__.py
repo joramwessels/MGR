e8247559a572e64c79129f85d323f3ea878513a2
joramwessels <joram.wessels@outlook.com>
2017-07-02 18:17:20 +0200

    Update __init__.py

--- bin/mgr/__init__.py
+++ bin/mgr/__init__.py
@@ -1,1 +1,5 @@
+from mgr_utils import log
+from mgr_utils import log_exception
+from mgr_utils import MGRException
+from mgr_utils import trackExceptions
 __all__ = ["preprocessing","training","testing","dataset","cnn","k2c2"]
